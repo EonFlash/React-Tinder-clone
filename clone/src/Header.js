f9fb5ef0379302a9dc8349fdb60514807f7f7bcb
EonFlash <gaurchintan@gmail.com>
2021-06-21 14:09:41 +0530

    Header Updated
    Header is updated with MaterialUI icons plus Icon API is used to add ripple effect

--- clone/src/Header.js
+++ clone/src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Header.css';
 
+//Profile icon
+import PersonIcon from '@material-ui/icons/Person';
+//Messenger icon
+import ForumIcon from '@material-ui/icons/Forum';
+
+//Icon button API This will add a ripple effect button
+import IconButton from '@material-ui/core/IconButton';
 function Header() {
     return (
         <div className='header'>
-            This is a header
+
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            <img className="header__logo" src="https://uxwing.com/wp-content/themes/uxwing/download/10-brands-and-social-media/tinder.png" alt="" />
+
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
+
         </div>
     )
 }
