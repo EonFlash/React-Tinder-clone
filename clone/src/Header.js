bbc029652ccdd15c5b81d4d448b77df0c64bf001
EonFlash <gaurchintan@gmail.com>
2021-06-21 12:41:42 +0530

    Getting started
    Going to start  building the header component

---
+++ clone/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            This is a header
+        </div>
+    )
+}
+
+export default Header
