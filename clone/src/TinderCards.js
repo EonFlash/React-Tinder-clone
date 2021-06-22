cbaab1451f3aa3ee2609f4356ec50f2576de8e1d
EonFlash <gaurchintan@gmail.com>
2021-06-22 22:51:58 +0530

    done
    Images are now rendering through the backend

--- clone/src/TinderCards.js
+++ clone/src/TinderCards.js
@@ -1,60 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css';
+import axios from "./axios";
 
 function TinderCards() {
 
-    const [people, setPeople] = useState([
+    const [people, setPeople] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
 
-
-        {
-            name: "Itachi",
-            url: "https://i0.wp.com/rankanime.com/wp-content/uploads/2020/07/itachi7.jpg?fit=1024%2C640&ssl=1",
-        },
-
-        {
-            name: "Bakugo",
-            url: "https://i.pinimg.com/originals/d7/fb/37/d7fb37c2cd9cf33d39439af73463f695.png",
-        },
-
-
-        {
-            name: "Sukuna",
-            url: "https://i.pinimg.com/474x/3f/71/59/3f7159db35d2989e231b161adf5635b9.jpg",
-        },
-
-        {
-            name: "Mitsuha",
-            url: "https://i.pinimg.com/originals/51/22/40/512240630643df5ae0a4e2d240974c9f.png",
-        },
-
-        {
-            name: "Taki",
-            url: "https://64.media.tumblr.com/a16d7fb35db2e061306e20fecf596f0e/tumblr_p1r39zxYmo1wfjbpvo1_1280.png",
-        },
-
-        {
-            name: "Nakiri",
-            url: "https://i.ytimg.com/vi/N__COfBvuaM/maxresdefault.jpg",
-        },
-
-        {
-            name: "Kakashi",
-            url: "https://cdn141.picsart.com/346192612051201.jpg",
-        },
-        {
-            name: "Nobara",
-            url: "https://s1.econotimes.com/assets/uploads/20201016ac08736315dd09ecb_th_1024x0.jpg",
-        },
-        {
-            name: "Gojo",
-            url: "https://i.ytimg.com/vi/dhQRxC023QA/maxresdefault.jpg",
-        },
-
-
-
-    ]);
+            setPeople(req.data);
+        }
+        fetchData();
+    }, [])
 
     const swiped = (direction, nameToDelete) => {
         console.log("Removing: " + nameToDelete);
@@ -76,7 +36,7 @@ function TinderCards() {
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >
 
-                        <div style={{ backgroundImage: "url(" + person.url + ")" }} className="card">
+                        <div style={{ backgroundImage: "url(" + person.imgUrl + ")" }} className="card">
 
                             <h3>{person.name}</h3>
 
